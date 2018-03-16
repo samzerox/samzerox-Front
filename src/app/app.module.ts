5ac12b5559c805a36af8a303b091031bf78e5a84
samzerox <sam.zerox88@gmail.com>
2018-03-16 14:30:58 -0600

    Agregamos bootstrap, rutas, los componentes home, proyectos y about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { APP_ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { AboutComponent } from './pages/about/about.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NopagefoundComponent,
+    HomeComponent,
+    ProyectosComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { AboutComponent } from './pages/about/about.component';
+
+
+
+const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent, data: {titulo: 'Home'} },
+    {path: 'proyectos', component: ProyectosComponent, data: {titulo: 'Proyectos'} },
+    {path: 'about', component: AboutComponent, data: {titulo: 'Sobre mi'} },
+    // { path: '**', component: NopagefoundComponent }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
