67fabedbcce1241b54a2c88419230c93afb90a9c
samzerox <sam.zerox88@gmail.com>
2018-04-05 18:17:10 -0500

    Adaptacion a tarjetas en ventana de proyectos terminada

--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
-  styles: [ `.container { margin : 50px auto; }`]
+  styles: [`.container { margin : 50px auto; }`]
 })
 export class ProyectosComponent implements OnInit {
 
