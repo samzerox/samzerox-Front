d486d6eae04dcf887e177eae00b5f16b95e98d92
samzerox <sam.zerox88@gmail.com>
2018-04-10 16:42:59 -0500

    Ventana About pendiente de menu lateral

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styles: []
+  styles: [`
+  .container-fluid {
+    background: url(../../../assets/img/bodyBack.png);
+    }
+  `]
 })
 export class AboutComponent implements OnInit {
 
