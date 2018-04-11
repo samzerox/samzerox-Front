c1aa63f9e9f5c687cf1154550dd9ad20e00cc859
samzerox <sam.zerox88@gmail.com>
2018-04-11 13:54:12 -0500

    El componente About se hizo Responsive, solo falta agregar collapse

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styles: [`
-  .container-fluid {
-    background: url(../../../assets/img/bodyBack.png);
-    }
-  `]
+  styles: []
 })
 export class AboutComponent implements OnInit {
 
