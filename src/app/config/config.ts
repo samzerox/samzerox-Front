0ecbdc3e1a55b62cb18a65ffd36a62af6d182d46
samzerox <sam.zerox88@gmail.com>
2018-04-23 20:18:18 -0500

    se agrego la ruta de heroku al archivo config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,6 @@
 
 
 
-export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://samzerox.herokuapp.com';
 
