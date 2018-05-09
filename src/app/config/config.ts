65856e9a195847042d06f02fe533a239f3dee015
samzerox <sam.zerox88@gmail.com>
2018-05-09 16:48:41 -0500

    Se cambio ruta a heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,6 +2,7 @@
 
 
 // export const URL_SERVICIOS = 'http://localhost:3000';
-export const URL_SERVICIOS = 'http://206.189.66.252:3000'; // ServidorDO
-// export const URL_SERVICIOS = 'https://samzerox.herokuapp.com';
+// export const URL_SERVICIOS = 'http://206.189.66.252:3000'; // ServidorDO
+export const URL_SERVICIOS = 'https://samzerox.herokuapp.com';
+
 
